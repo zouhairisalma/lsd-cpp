607fd1b61e6f495265b90356fd620eaa04b527fd
redone <redouanemahjoubii@gmail.com>
2022-10-06 12:29:42 +0100

    activate some tests

--- ses1/main.c
+++ ses1/main.c
@@ -7,14 +7,14 @@ int main()
     int m;
     int n;
 
-	//printf("%d\n", 1 >> 1);
-	//printf("Enter a number <= 255 to print its binary: ");
-	//scanf("%hhu", &byte);
-	//print_bits(byte);
-	//print_bits_flashy(byte);
-	//printf("Enter an integer to check its msb: ");
-	//scanf("%d", &num);
-	//check_msb(num);
+	printf("%d\n", 1 >> 1);
+	printf("Enter a number <= 255 to print its binary: ");
+	scanf("%hhu", &byte);
+	print_bits(byte);
+	print_bits_flashy(byte);
+	printf("Enter an integer to check its msb: ");
+	scanf("%d", &num);
+	check_msb(num);
 
 	printf("Enter two integers n then m, to print the nth byte of m");
     scanf("%d%d", &n, &m);
