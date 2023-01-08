913fb77842528ffaabfed4462eab5677d086364d
ZouhairiSalma <92038490+zouhairisalma@users.noreply.github.com>
2023-01-08 23:09:20 +0100

    Create shortest_path.hpp

---
+++ cpp_tps/tp1/shortest_path.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*PROTOTYPES*/
+vector<int> Adjacents(unsigned i,vector<vector<int>> G ,unsigned n);
+int Search(unsigned i,vector <int> V);
+vector <int> shortestpath (unsigned n,vector<vector<int>> G,unsigned z );
